7450fc3ba5454f22d6271f2228f8da16832e25a5
VzGriffin <114660162+VzGriffin@users.noreply.github.com>
2022-10-06 11:19:06 +0100

    exercise attempt

--- src/Week2/Week02.java
+++ src/Week2/Week02.java
@@ -1,5 +1,7 @@
 package Week2;
 
+import helpers.InputReader;
+
 public class Week02 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
@@ -15,5 +17,21 @@ public class Week02
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
+        /*
+        int lenth = InputReader.getInt("Enter Lenth ");
+        int Hight = InputReader.getInt("Enter Hight ");
+        int permieter = (lenth + Hight)*2;
+        int area = lenth * Hight;
+        System.out.println("The Permiter is " + permieter);
+        System.out.println("the Area is " + area);
+        */
+        int N1 = InputReader.getInt("Enter Number 1 ");
+        int N2 = InputReader.getInt("Enter Number 2 ");
+        int total = InputReader.getInt("Enter total");
+        int amount = N1 + N2;
+        int avrage = (total / amount);
+        System.out.println("The Avrage is " + amount);
+        System.out.println("The Avrage is " + avrage);
+
     }     
 }
